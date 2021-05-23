866ba5c81af9cfb204b4fe928ddbe76b0b8bc5c4
Dmitry <11094711+dmi3mis@users.noreply.github.com>
2021-05-23 14:06:48 +0300

    updated index.php

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 <body>
 
 <?php echo "Hello World!"; ?>
-<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?>
-
-<?php if($_ENV["COLOR"]) {?><h3>This site has color: <?php echo $_ENV["COLOR"]; ?>
-
+<?php
+echo 'This site is: ' .$_ENV["COLOR"] . '!';
+?>
 </body>
 </html>
